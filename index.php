7c00d868de2a4f852a088a68831865cf179e9f00
Aalian2241 <aalian.2241@gmail.com>
2023-04-19 10:07:46 +0500

    code modularized

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Website</title>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
+    <link rel="import" href="Components/Landing/LandingSection.html">
+    <link rel="import" href="Components/Services/Services.html">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">	<link rel="stylesheet" href="Components/Landing Section/LandingPage.css">
+    <link rel="stylesheet" href="Components/Services/Services.css">
+</head>
+<body>
+    <div class="container-fluid">
+       
+                
+        
+        
+        <iframe src="Components/Landing/LandingSection.html" frameborder="0" scrolling="no" style="width: 100%; height:600px;"></iframe>
+        <div class="divider"></div>
+        <iframe src="Components/Services/Services.html" frameborder="0" scrolling="no" style="width: 100%; height:600px;"></iframe>
+
+        
+    </div>
+	
+    <main>
+		<!-- Page content goes here -->
+	</main>
+	
+	<footer>
+		<!-- Footer content goes here -->
+	</footer>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script></body>
+</html>
